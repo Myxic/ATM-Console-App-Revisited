d93be00897651f8fbd5c2b5ffe6a2eb7b2741796
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-12-05 21:49:22 +0100

    Initial commit

---
+++ ATM Console App Revisited/DelegateHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ATM_Console_App_Revisited
+{
+    public delegate void MyDelegateHandler();
+    public class DelegateHandler
+    {
+        public DelegateHandler()
+        {
+        }
+    }
+}
+
